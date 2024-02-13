01fa42152943d655decacda6260014403f27b41a
Felipe <felipejoseph45@hotmail.com>
2024-02-13 16:31:25 -0300

    criação da pasta terraform/main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "comentarios_app" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "comentarios-app-instance"
+  }
+  
+}
+output "public_ip" {
+  value = aws_instance.comentarios_app.public_ip
+}
