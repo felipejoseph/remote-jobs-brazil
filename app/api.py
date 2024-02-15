19408ac2de2cc4f5807cf01575e113e1953088a8
Felipe <felipejoseph45@hotmail.com>
2024-02-15 10:48:19 -0300

    teste

--- app/api.py
+++ app/api.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from flask import jsonify
+from flask import request
+
 
 app_name = 'comentarios'
 app = Flask(app_name)
@@ -6,43 +9,43 @@ app.debug = True
 
 comments = {}
 
+
 @app.route('/api/comment/new', methods=['POST'])
 def api_comment_new():
-    try:
-        request_data = request.get_json()
-        if not all(key in request_data for key in ['email', 'comment', 'content_id']):
-            raise ValueError("Missing required fields")
+    request_data = request.get_json()
 
-        email = request_data['email']
-        comment = request_data['comment']
-        content_id = str(request_data['content_id'])
+    email = request_data['email']
+    comment = request_data['comment']
+    content_id = '{}'.format(request_data['content_id'])
 
-        new_comment = {'email': email, 'comment': comment}
+    new_comment = {
+            'email': email,
+            'comment': comment,
+            }
 
-        if content_id in comments:
-            comments[content_id].append(new_comment)
-        else:
-            comments[content_id] = [new_comment]
+    if content_id in comments:
+        comments[content_id].append(new_comment)
+    else:
+        comments[content_id] = [new_comment]
 
-        message = f"Comment created and associated with content_id {content_id}"
-        response = {'status': 'SUCCESS', 'message': message}
-        return jsonify(response)
-
-    except Exception as e:
-        response = {'status': 'ERROR', 'message': str(e)}
-        return jsonify(response), 400
+    message = 'comment created and associated with content_id {}'.format(content_id)
+    response = {
+            'status': 'SUCCESS',
+            'message': message,
+            }
+    return jsonify(response)
 
 
 @app.route('/api/comment/list/<content_id>')
 def api_comment_list(content_id):
-    content_id = str(content_id)
+    content_id = '{}'.format(content_id)
+
     if content_id in comments:
         return jsonify(comments[content_id])
     else:
-        message = f"Content_id {content_id} not found"
-        response = {'status': 'NOT-FOUND', 'message': message}
+        message = 'content_id {} not found'.format(content_id)
+        response = {
+                'status': 'NOT-FOUND',
+                'message': message,
+                }
         return jsonify(response), 404
-
-
-if __name__ == "__main__":
-    app.run()
